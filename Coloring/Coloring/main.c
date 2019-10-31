d93e17ddd09e3ef662eed3a54635a2b7a0c78d70
Blohh <agnieszka.omernik@gmail.com>
2019-10-31 20:59:07 +0100

    Add functions that: count connections in graph, check if graph is empty, check if graph is full

--- Coloring/Coloring/main.c
+++ Coloring/Coloring/main.c
@@ -19,7 +19,29 @@ void fill2DimentionalTable(int **tab, int size)
 		for (int j = 0; j < size; j++)
 			scanf("%d", &tab[i][j]);
 }
-
+//counts connections in graph
+int countConnectionsInGraph(int **graph, int graph_size)
+{
+	int count_of_connections;
+	for (int i = 0; i < graph_size; i++)
+		for (int j = 0; j < graph_size; j++)
+			count_of_connections += graph[i][j];
+	return count_of_connections;
+}
+//checks if graph is full
+enum boolean isGraphFull(int **graph, int graph_size, int count_of_connections)
+{
+	int countOfConnectionsInFullGraph = graph_size * (graph_size - 1);
+	if (count_of_connections == countOfConnectionsInFullGraph) return true;
+	else return false;
+}
+//checks if graph is empty
+enum boolean isGraphEmpty(int **graph, int graph_size, int count_of_connections)
+{
+	int countOfConnectionsInEmptyGraph = 0;
+	if (count_of_connections == countOfConnectionsInEmptyGraph) return true;
+	else return false;
+}
 int main()
 {
 	int graph_length = 0,
